9769919d33899aeaa1d3524bd233e638431fd974
Nofar Belo <nofarbelo@gmail.com>
2021-04-04 14:56:48 +0300

    Update s3.tf
    public

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -68,7 +68,7 @@ resource "aws_s3_bucket" "data_science" {
 
 resource "aws_s3_bucket" "logs" {
   bucket = "${local.resource_prefix.value}-logs"
-  acl    = "log-delivery-write"
+  acl    = "public"
   versioning {
     enabled = true
   }
